61e6219f17d3f686e5e42a95a592a07131214d79
Saltanat2020 <74146176+Saltanat2020@users.noreply.github.com>
2021-01-28 23:24:13 -0500

    I did update my code, i added 3 more sentences

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,6 +1,16 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("My first commit");
+        
+                System.out.println("Mohammed Alattar");
+        System.out.println("Daria Volkau");
+        System.out.println("Abdullah Irfan");
+        System.out.println("Mustafa Tanis");
+        System.out.println("Abdujilil Ahat");
+        
+        
+        
+        
     }
 
 }
